a83609d99667ed2f119a5f877e5130fed714ec53
gardocki1095 <46058523+gardocki1095@users.noreply.github.com>
2018-12-21 17:56:09 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class Artist(models.Model):
 
 class Album(models.Model):
     name = models.CharField("album",max_length=50)
-    artist = models.ForeignKey(Artist)
+    artist = models.ForeignKey(Artist,on_delete=models.PROTECT)
 
 class ArtistForm(forms.ModelForm):
     class Meta:
