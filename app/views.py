5a5deeac5ba766df293fbfb493e2144ea2bdbeca
gardocki1095 <46058523+gardocki1095@users.noreply.github.com>
2018-12-21 19:31:49 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -19,11 +19,11 @@ def artistcreate(request):
         
 def artists(request):
     artists = Artist.objects.all()
-    return render('app/artists.html',{'artists':artists})
+    return render(request,'app/artists.html',{'artists':artists})
 
 def artistdetails(request, id):
     artist = Artist.objects.get(pk = id)
-    return render_to_response('templates/app/artistdetails.html',{'artist':artist})
+    return render(request,'templates/app/artistdetails.html',{'artist':artist})
 
 def home(request):
     """Renders the home page."""
